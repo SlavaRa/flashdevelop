79e44a1ca150b87fe0f33212bf0b457c068d4e5f
SlavaRa <ratdg@ya.ru>
2016-03-03 21:17:26 +0300

    Small Cleanup

--- SetVersion.sh
+++ SetVersion.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 target_dir=$1
 cd $target_dir
-mono External/Tools/SetVersion/SetVersion.exe FlashDevelop/Properties/AssemblyInfo.cs fake-param
+mono External/Tools/SetVersion/SetVersion.exe FlashDevelop/Properties/AssemblyInfo.cs "%APPVEYOR_BUILD_NUMBER%"
